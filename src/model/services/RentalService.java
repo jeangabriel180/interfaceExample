cda0bbfe2f669f89f9ab009126a3d57a316d7fd2
Jean Gabriel <jeangabriel180@hotmail.com>
2022-03-05 11:58:30 -0300

    Adicionando projeto sem interface

---
+++ src/model/services/RentalService.java
@@ -0,0 +1,31 @@
+package model.services;
+
+import model.entities.CarRental;
+import model.entities.Invoice;
+
+public class RentalService {
+
+    private Double pricePerDay;
+    private Double pricerPerHour;
+    private BrazilTaxService taxService;
+
+    public RentalService(Double pricePerDay, Double pricerPerHour, BrazilTaxService taxService) {
+        this.pricePerDay = pricePerDay;
+        this.pricerPerHour = pricerPerHour;
+        this.taxService = taxService;
+    }
+
+    public void processInvoice(CarRental carRental) {
+        long t1 = carRental.getStart().getTime();
+        long t2 = carRental.getFinish().getTime();
+        double hours = (double) (t2 - t1) / 1000.0 / 60.0 / 60.0;
+        double basicPayment;
+        if (hours <= 12.0) {
+            basicPayment = Math.ceil(hours) * pricerPerHour;
+        } else {
+            basicPayment = Math.ceil(hours / 24.0) * pricePerDay;
+        }
+        double tax = taxService.tax(basicPayment);
+        carRental.setInvoice(new Invoice(basicPayment, tax));
+    }
+}
